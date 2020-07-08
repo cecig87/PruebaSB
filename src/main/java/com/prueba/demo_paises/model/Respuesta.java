a7ca9ffac35522844571c8cc84a7892ee5eab800
CeciCode <cecicode20@gmail.co>
2020-07-07 22:15:15 -0300

    Se agregó el archivo Respuesta.java

---
+++ src/main/java/com/prueba/demo_paises/model/Respuesta.java
@@ -0,0 +1,13 @@
+package com.prueba.demo_paises.model;
+
+public class Respuesta {
+   private String error; 
+
+public String getError(){
+    return error;
+}
+
+public void setError(String error) {
+ this.error = error;
+}
+}
