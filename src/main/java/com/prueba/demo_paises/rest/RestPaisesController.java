b438613c0194f8800be371fe8b3fd60bef1450d4
CeciCode <cecicode20@gmail.co>
2020-08-07 21:08:50 -0300

    Se agregó CrossOrigin

--- src/main/java/com/prueba/demo_paises/rest/RestPaisesController.java
+++ src/main/java/com/prueba/demo_paises/rest/RestPaisesController.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200") //, maxAge = 3600)
 @RestController
 @RequestMapping("/paises")
 public class RestPaisesController {
